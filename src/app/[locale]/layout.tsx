a54f58aaedecc42ddd8ad3ffc1ab76e4da51eaf6
cuongnm <cuong2th2@gmail.com>
2024-10-06 17:59:06 +0700

    add font

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,15 +5,9 @@ import StoreProvider from '../store-provider';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 
-const geistSans = localFont({
-  src: '../fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: '../fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const inter = localFont({
+  src: '../fonts/inter-variable.ttf',
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
@@ -31,7 +25,7 @@ export default async function RootLayout({
   const messages = await getMessages();
   return (
     <html lang={locale}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${inter.variable} font-inter`}>
         <StoreProvider>
           <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
         </StoreProvider>
